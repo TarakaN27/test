a785f54a0d1594798254d61ea389bbb9efca3924
TarakaN27 <Elya.Gorbachev27@yandex.by>
2021-10-16 17:11:36 +0300

    TarakaN27

--- stage2.2/js/script.js
+++ stage2.2/js/script.js
@@ -1,8 +1,3 @@
-var object = "";
-fetch('https://jsonplaceholder.typicode.com/todos/')
-	.then(response => response.json())
-	.then(json => {object = json});
-	
 $(document).mouseup(function (e){
 	var div = $(".dropdown .show");
 	if (!div.is(e.target) && div.has(e.target).length === 0) {
@@ -17,24 +12,6 @@ $(window).on('resize scroll', function(){
 	$("input.dropbtn").blur();
 });
 
-$("input.dropbtn").click(function(){
-	var height_dropdown = 212;
-	var elm = $(this).parent();
-	var input_top = elm.offset().top;
-	var input_bottom = $(window).height() - (input_top + elm.outerHeight());
-	var input_left = elm.offset().left;
-	var input_right = $(window).width() - (input_left + elm.outerWidth());
-	var position = "";
-	
-	if(input_left > height_dropdown) {position = "left";}
-	if(input_right > height_dropdown) {position = "right";}
-	if(input_top > height_dropdown) {position = "top";}
-	if(input_bottom > height_dropdown) {position = "bottom";}
-	
-	$(this).parent().find(".dropdown-content").addClass("show").attr("data-position", position);
-	$(this).val('');
-});
-
 $('body').on('click', 'span.list-item', function(){
 	var selected = $(this).text();
 	$(this).parents(".dropdown").find(".dropdown-content").removeClass("show");
@@ -42,20 +19,67 @@ $('body').on('click', 'span.list-item', function(){
 	$(this).parents(".dropdown").find("input.dropbtn").attr("data-prev-selected", selected);
 });
 
-function filterFunction() {
-	var input = $("input.dropbtn").val();
-	var z = 0;
-	$(".dropdown-list-items").empty();
-	for (i = 0; i < object.length; i++) {
-		var txtValue = object[i].title;
- 		if (txtValue.match("^"+input)) {
-			var elm = $(".dropdown-list-items").append('<span class="list-item">'+txtValue+'</span>');
-			z++;
+$("input.dropbtn, .dropicon").click(function(){
+	$(this).parent().find("input.dropbtn").val('').focus();
+});
+
+class Dropdown {
+	
+	constructor(url) {
+		this.url = url;
+		fetch(this.url)
+					.then(response => response.json())
+					.then(json => {this.object = json});
+	}
+	
+	getFilteredList(block_id){
+		this.filtered_list = [];
+		this.input = $("#"+block_id).find("input.dropbtn").val();
+		this.z = 0;
+		for (this.i = 0; this.i < this.object.length; this.i++) {
+			this.txtValue = this.object[this.i].title;
+			if (this.txtValue.match("^"+this.input)) {
+				this.filtered_list.push(this.txtValue);
+				this.z++;
+			}
 		}
 	}
-	if(z > 5) {
-		$(".dropdown-list-items").addClass("scroll");
-	} else {
-		$(".dropdown-list-items").removeClass("scroll");
+	
+	showDropdown(block_id){
+		$("#"+block_id).find(".dropdown-list-items").empty();
+		this.filtered_list.forEach(function(item){
+			$("#"+block_id).find(".dropdown-list-items").append('<span class="list-item">'+item+'</span>');
+		})
+		if(this.z > 5) {
+			$("#"+block_id).find(".dropdown-list-items").addClass("scroll");
+		} else {
+			$("#"+block_id).find(".dropdown-list-items").removeClass("scroll");
+		}
+		$("#"+block_id).find(".dropdown-content").addClass("show");
 	}
-};
+	
+	setPosition(block_id, height_item){
+		if(this.z > 5) {
+			this.height_dropdown = height_item*5;
+		} else {
+			this.height_dropdown = this.z*height_item;
+		}
+		this.position = this.height_dropdown*-1;
+		this.elm = $("#"+block_id);		
+		this.input_top = this.elm.offset().top;
+		this.input_bottom = $(window).height() - (this.input_top + this.elm.outerHeight());
+		if(this.input_top > this.height_dropdown) {
+			this.elm.find(".dropdown-content").css('top', this.position);
+		}
+	}
+}
+
+var obj = new Dropdown('https://jsonplaceholder.typicode.com/todos/');
+
+$("input.dropbtn, .dropicon").on("keyup click",function(){
+	var block_id = $(this).parents(".dropdown").attr("id");
+	obj.getFilteredList(block_id);
+	obj.showDropdown(block_id);
+	var height_item = $("#"+block_id).find(".list-item").outerHeight();
+	obj.setPosition(block_id,height_item);
+})
